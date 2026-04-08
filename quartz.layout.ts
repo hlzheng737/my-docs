ae1c1571378786cbd2a3171f67651bbda239f50a
xiaoliangge <hlzheng737@gmail.com>
2026-04-08 20:29:29 +0800

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -67,7 +67,7 @@ export const defaultContentPageLayout: PageLayout = {
     }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
-  ],,
+  ],
 }
 
 // components for pages that display lists of pages  (e.g. tags or folders)
